c2e63f772e46dc6b7056ea9a7f5106d2657cd4b9
G1gg1L3s <mr.a678i@gmail.com>
2020-12-25 14:29:20 +0200

    Add event loop and parser into main

--- main.go
+++ main.go
@@ -1,24 +1,45 @@
 package main
 
-import "fmt"
-
 import (
+	"bufio"
+	"fmt"
+	"os"
+
 	engine "./eventLoop"
-	. "./command"
+
+	. "./commands"
+	. "./parser"
 )
 
 func main() {
 	ev := new(engine.EventLoop)
 	ev.Start()
 
-	ev.Post(CommandFunc(func(h Handler) {
-		fmt.Println("1")
-	}))
-	ev.Post(CommandFunc(func(h Handler) {
-		fmt.Println("2")
-	}))
-	ev.Post(CommandFunc(func(h Handler) {
-		fmt.Println("3")
-	}))
+	if len(os.Args) != 2 {
+		ev.Post(&ErrorCommand{Msg: fmt.Sprintf("Usage: %s <file>\n", os.Args[0])})
+	} else {
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			ev.Post(&ErrorCommand{Msg: err.Error()})
+		} else {
+			reader := bufio.NewReader(file)
+			parser := NewParser(reader)
+			cmnds := parser.Parse()
+			for _, cmd := range cmnds {
+				ev.Post(cmd)
+			}
+		}
+
+	}
+
+	// ev.Post(CommandFunc(func(h Handler) {
+	// 	fmt.Println("1")
+	// }))
+	// ev.Post(CommandFunc(func(h Handler) {
+	// 	fmt.Println("2")
+	// }))
+	// ev.Post(CommandFunc(func(h Handler) {
+	// 	fmt.Println("3")
+	// }))
 	ev.AwaitFinish()
 }
